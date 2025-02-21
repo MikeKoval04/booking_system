a1065daffc5b8651ff9d680fb3fcc45a299ba6aa
Михаил <mishakoval2013@gmail.com>
2025-02-21 18:35:10 +0300

    debug

--- sw.js
+++ sw.js
@@ -3,6 +3,7 @@ self.addEventListener('install', (event) => {
       caches.open('v1').then((cache) => {
         return cache.addAll([
           '/',
+          'index.html',
           '/styles.css',
           '/scripts.js',
           // Добавьте другие ресурсы, которые должны быть кэшированы
@@ -15,12 +16,20 @@ self.addEventListener('install', (event) => {
     event.respondWith(
       caches.match(event.request).then((response) => {
         return response || fetch(event.request).then((fetchResponse) => {
+          // Проверяем, если это редирект, то не кэшируем
+          if (fetchResponse.redirected) {
+            return fetchResponse;
+          }
           return caches.open('v1').then((cache) => {
             cache.put(event.request, fetchResponse.clone());
             return fetchResponse;
           });
+        }).catch((error) => {
+          console.error('Ошибка при запросе:', error);
+          return caches.match('/offline.html'); // Подставьте свой offline-ресурс
         });
       })
     );
   });
+  
   
